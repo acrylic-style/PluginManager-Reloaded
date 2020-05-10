ea16d2edd56e51fd5afbbe3bf9695e7dc70ec5a6
acrylic-style <me@acrylicstyle.xyz>
2020-05-11 03:29:30 +0900

    update to api version 0.4.0

--- src/main/java/xyz/acrylicstyle/plugin/PluginManagerConfig.java
+++ src/main/java/xyz/acrylicstyle/plugin/PluginManagerConfig.java
@@ -1,27 +1,20 @@
 package xyz.acrylicstyle.plugin;
 
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.InvalidConfigurationException;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import xyz.acrylicstyle.tomeito_api.providers.ConfigProvider;
 import xyz.acrylicstyle.tomeito_api.providers.LanguageProvider;
 
-import java.io.IOException;
-
 public class PluginManagerConfig extends ConfigProvider {
-    public PluginManagerConfig(String path) throws IOException, InvalidConfigurationException {
+    public PluginManagerConfig(String path) {
         super(path);
     }
 
     @NotNull
     @Contract("_ -> new")
     public static PluginManagerConfig newInstance(@NotNull String path) {
-        try {
-            return new PluginManagerConfig(path);
-        } catch (IOException | InvalidConfigurationException e) {
-            throw new RuntimeException(e);
-        }
+        return new PluginManagerConfig(path);
     }
 
     public String getLanguage() {
